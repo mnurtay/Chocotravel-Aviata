e922d17391419576b630ad3af8ad530e4d8cf089
Seisembayev <37009712+Seisembayev@users.noreply.github.com>
2018-07-24 18:31:46 +0600

    Update parser.py

--- parser.py
+++ parser.py
@@ -1,15 +1,24 @@
 import xml.dom.minidom
 
 dom = xml.dom.minidom.parse("fare_rules.xml")
+book = xml.dom.minidom.parse("booking.xml")
 
 dom.normalize()
+book.normalize()
 
+#FARE-RULES
 SubSection = dom.getElementsByTagName("SubSection")[5]
 
-print("name = " + SubSection.nodeName)
-print("SubTitle = " + SubSection.getAttribute("SubTitle"))
+#print("name = " + SubSection.nodeName)
+#print("SubTitle = " + SubSection.getAttribute("SubTitle"))
 
 Paragraph = SubSection.getElementsByTagName("Paragraph")[0]
 
 Text = Paragraph.getElementsByTagName("Text")[0]
 print("text = " + Text.firstChild.nodeValue)
+
+#BOOKING
+Status = book.getElementsByTagName("Status")[2]
+
+#print("name = " + Status.nodeName)
+print("value = " + Status.childNodes[0].nodeValue)
