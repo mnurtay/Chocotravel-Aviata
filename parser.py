be185a91f7a9f49a5fc848ab7db2cfac74c79b7f
MeBr0 <aza17e@gmail.com>
2018-07-25 14:29:57 +0600

    functions for tarif, farerule-scat, farerule1-original

--- parser.py
+++ parser.py
@@ -1,6 +1,6 @@
 import xml.dom.minidom
 
-def get_status(xml_dom):							# get current status fro booking.xml dom
+def get_status(xml_dom):							# get status from booking.xml dom_object
 	status = xml_dom.getElementsByTagName("Status")[2]
 
 	# print("name = " + Status.nodeName)
@@ -10,7 +10,7 @@ def get_status(xml_dom):							# get current status fro booking.xml dom
 	return status
 
 
-def get_rules(xml_dom):								# get fare rule text from fare_rules.xml dom
+def get_rules(xml_dom):								# get fare_rule_text from fare_rules.xml dom_object
 	subSection = xml_dom.getElementsByTagName("SubSection")[5]
 
 	# print("name = " + SubSection.nodeName)
@@ -22,19 +22,91 @@ def get_rules(xml_dom):								# get fare rule text from fare_rules.xml dom
 	
 	return rules.firstChild.nodeValue
 
-def get_xml_dom(text):								# get dom object from xml file
-	xml_dom = xml.dom.minidom.parse(text + '.xml')
+def get_xml_dom(file_name):								# get dom_object from xml_file
+	xml_dom = xml.dom.minidom.parse(file_name + '.xml')
 	xml_dom.normalize()
 
 	return xml_dom
+
+def get_tarif(fare_rule_text):						# get tarif`s name from fare_rule_text
+	words = fare_rule_text.split(' ')
+
+	try:
+		first_word = words[1].split('.')[2]			# get first_word of fare_rule_texxt
+	except:
+		first_word = ''
+
+	if first_word == 'PERMITTED':					# 'Flexible' tarif`s text begins with 'PERMITTED'
+		tarif = 'Flexible'
+
+	elif first_word == 'CHANGES':					# other tarif`s text begins with 'CHANGES'
+		try:										# try to define minutes
+			minutes = int(words[4])
+		except:
+			minutes = 0
+
+		try:										# try to define first_charge
+			first_charge = int(words[11].split('\n')[0])
+		except:
+			first_charge = 0
+
+		try:										# try to define second_charge
+			second_charge = int(words[26])
+		except:
+			second_charge = 0
+
+		print(minutes, first_charge, second_charge)
+
+		if minutes != 0 and first_charge != 0 and second_charge != 0:	# define tarif by switch case
+			tarif = switch_tarif(minutes, first_charge, second_charge)
+
+		else:
+			tarif = None
+	else:
+		tarif = None
+
+	return tarif
+
+def switch_tarif(minutes, first, second):			# get tarif`s name from minutes, first_charge and second_charge
+	if minutes == 90:
+		if first == 10 and second == 15:
+			tarif = 'Bet/Economy'
+
+		elif first == 15 and second == 20:
+			tarif = 'Bet/Semi'
+
+		elif first == 20 and second == 25:
+			tarif = 'Bet/SStandart'
+
+		elif first == 25 and second == 30: # except APROMO etc
+			tarif = 'Bet/SPromo'
+
+	elif minutes == 180:
+		if first == 10 and second == 15:
+			tarif = 'Int/Economy'
+
+		elif first == 15 and second == 20:
+			tarif = 'Int/Semi'
+
+		elif first == 20 and second == 25:
+			tarif = 'Int/SRestricted'
+
+		elif first == 25 and second == 30: # except APROMO etc
+			tarif = 'Int/SPromo'
+
+	return tarif
 
 def main():
 	fare_rules = get_xml_dom('fare_rules')
 	booking = get_xml_dom('booking')
 
-	print(get_rules(fare_rules))
+	rules = get_rules(fare_rules)
 
-	print(get_status(booking))
+	tarif = get_tarif(rules)
+	# tarifs: 'Flexible', 'Bet/Economy', 'Bet/Semi', 'Bet/SStandart', 'Bet/SPromo'
+	#		  'Int/Economy', 'Int/Semi', 'Int/SRestricted', 'Int/SPromo'
+
+	print(tarif)
 
 if __name__ == '__main__':
 	main()
