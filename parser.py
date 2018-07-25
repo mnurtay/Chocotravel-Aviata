28182e85666fec598ecc8329ae68532b60397456
MeBr0 <aza17e@gmail.com>
2018-07-25 12:13:06 +0600

    divinding into functions

--- parser.py
+++ parser.py
@@ -1,24 +1,40 @@
 import xml.dom.minidom
 
-dom = xml.dom.minidom.parse("fare_rules.xml")
-book = xml.dom.minidom.parse("booking.xml")
+def get_status(xml_dom):										# get current status fro booking.xml dom
+	status = xml_dom.getElementsByTagName("Status")[2]
 
-dom.normalize()
-book.normalize()
+	# print("name = " + Status.nodeName)
 
-#FARE-RULES
-SubSection = dom.getElementsByTagName("SubSection")[5]
+	status = status.childNodes[0].nodeValue
 
-#print("name = " + SubSection.nodeName)
-#print("SubTitle = " + SubSection.getAttribute("SubTitle"))
+	return status
 
-Paragraph = SubSection.getElementsByTagName("Paragraph")[0]
 
-Text = Paragraph.getElementsByTagName("Text")[0]
-print("text = " + Text.firstChild.nodeValue)
+def get_rules(xml_dom):											# get fare rule text from fare_rules.xml dom
+	subSection = xml_dom.getElementsByTagName("SubSection")[5]
 
-#BOOKING
-Status = book.getElementsByTagName("Status")[2]
+	# print("name = " + SubSection.nodeName)
+	# print("SubTitle = " + SubSection.getAttribute("SubTitle"))
 
-#print("name = " + Status.nodeName)
-print("value = " + Status.childNodes[0].nodeValue)
+	paragraph = subSection.getElementsByTagName("Paragraph")[0]
+
+	rules = paragraph.getElementsByTagName("Text")[0]
+	
+	return rules.firstChild.nodeValue
+
+def get_xml_dom(text):											# get dom object from xml file
+	xml_dom = xml.dom.minidom.parse(text + '.xml')
+	xml_dom.normalize()
+
+	return xml_dom
+
+def main():
+	fare_rules = get_xml_dom('fare_rules')
+	booking = get_xml_dom('booking')
+
+	print(get_rules(fare_rules))
+
+	print(get_status(booking))
+
+if __name__ == '__main__':
+	main()
