308a91288a2afb572a7e39ed87e84d4789d5329c
NurMaks <nurtaims@gmail.com>
2018-07-31 15:37:29 +0600

    Создал Turkish AirLines

---
+++ turkish.py
@@ -0,0 +1,10 @@
+class Turkish:
+    
+    def __init__(self, data):)
+        self.totalFare = data["totalFare"]
+        self.baseFare = data["baseFare"]
+        self.taxes = data["taxes"]
+        self.dates = data["dates"]
+        self.company_codes = data["company_codes"]
+        self.currencies = data["currencies"]
+        self.rules = data["rules"]
