52c309cb4c069aa754783cc2522637676f279648
NurMaks <nurtaims@gmail.com>
2018-07-31 17:40:24 +0600

    Test version

--- turkish.py
+++ turkish.py
@@ -6,18 +6,21 @@ class Turkish:
     def __init__(self, data):
         self.totalFare = data["totalFare"]
         self.baseFare = data["baseFare"]
+        self.totalTaxes = self.totalFare-self.baseFare
         self.taxes = data["taxes"]
         self.dates = data["dates"]
         self.company_codes = data["company_codes"]
         self.currencies = data["currencies"]
         self.rules = data["rules"]
-
-        self.non_ref_fare = 0
     
     def calculate(self):
+        if not self.dates[0] < self.dates[1]:
+            return None
         rules = self.rules.split("\n")
         beforeDeparture = []
         penalty = []
+        non_refunded_taxes = 0
+        refunded_taxes = 0
         check = 0
         for rule in rules:
             if "REISSUE/REVALIDATION" in rule:
@@ -29,11 +32,21 @@ class Turkish:
                 check = 2
             if check == 1:
                 beforeDeparture.append(rule)
-        self.non_ref_fare = self.__get_Exchange_Rates(penalty)
-        print(self.non_ref_fare)
-
+        non_ref_fare = round(self.__get_Exchange_Rates(penalty), 1)
+        if non_refunded_taxes == 0:
+            refunded_taxes = self.totalTaxes
         print("\n")
-        return None
+        output = {
+            'total taxes': self.totalTaxes,
+            'non_refundable taxes': non_refunded_taxes,
+            'penalty': str(penalty[1])+penalty[0]+" or "+str(non_ref_fare)+"KZT",
+            'refunded_fare': self.baseFare - non_ref_fare,
+            'refunded_taxes':  refunded_taxes,
+            'refunded_total': (self.baseFare-non_ref_fare)+refunded_taxes,
+            'name': "Turkish AirLine",
+            'currency': self.currencies
+        }
+        return output
 
     def __get_Exchange_Rates(self, data):
         site = requests.get('https://prodengi.kz/currency/')
