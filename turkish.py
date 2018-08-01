02a470bf7fa93e0c9bd323594589748a0e86aef7
NurMaks <nurtaims@gmail.com>
2018-08-01 16:04:46 +0600

    Turkish Debug version

--- turkish.py
+++ turkish.py
@@ -12,34 +12,59 @@ class Turkish:
         self.company_codes = data["company_codes"]
         self.currencies = data["currencies"]
         self.rules = data["rules"]
+        self.ticketStatus = "OK"
     
-    def calculate(self):
-        if not self.dates[0] < self.dates[1]:
-            return None
+    def calculate(self):            
         rules = self.rules.split("\n")
-        beforeDeparture = []
+        cancellation = []
         penalty = []
-        non_refunded_taxes = 0
+        non_refunded_tax = 0
+        non_refunded_taxes = []
         refunded_taxes = 0
         check = 0
-        for rule in rules:)
-            if "REISSUE/REVALIDATION" in rule:
-                penalty.append(rule.split()[1])
-                penalty.append(float(rule.split()[2]))
-            if "A-BEFORE DEPARTURE" in rule:
+        for rule in rules:
+            if "CHANGES" in rule:
+                check = 1
+            elif "CANCELLATIONS" in rule:
+                check = 2
+            if check == 2:
+                cancellation.append(rule)
+        
+        check = 0
+        for rule in cancellation:
+            if "NO-SHOW" in rule and "NON-REFUNDABLE" in rule:
+                if self.ticketStatus == "NO-SHOW":
+                    if not self.dates[0] < self.dates[1]:
+                        return None
+            if "SURCHARGE" in rule or "TAX" in rule:
+                for tax in self.taxes:
+                    if tax["Type"] in rule:
+                        non_refunded_taxes.append(tax)
+            if "BEFORE DEPARTURE" in rule:
                 check = 1
             elif "AFTER DEPARTURE" in rule:
                 check = 2
             if check == 1:
-                beforeDeparture.append(rule)
+                if "CANCEL/REFUND" in rule:
+                    for r in rule.split():
+                        if r=="USD" or r=="EUR":
+                            penalty.append(r)
+                        try:
+                            penalty.append(float(r))
+                        except:
+                            pass
+        non_refunded_tax = self.__non_refundable_taxes(non_refunded_taxes)
+
         non_ref_fare = round(self.__get_Exchange_Rates(penalty), 1)
+
         if non_refunded_taxes == 0:
             refunded_taxes = self.totalTaxes
-        print("\n")
+        else:
+            refunded_taxes = self.totalTaxes - non_refunded_tax
+        
         output = {
-            'total taxes': self.totalTaxes,
-            'non_refundable taxes': non_refunded_taxes,
-            'penalty': str(penalty[1])+penalty[0]+" or "+str(non_ref_fare)+"KZT",
+            'non_refundable taxes': non_refunded_tax,
+            'penalty': str(penalty[1])+" "+penalty[0]+" or "+str(non_ref_fare)+" "+"KZT",
             'refunded_fare': self.baseFare - non_ref_fare,
             'refunded_taxes':  refunded_taxes,
             'refunded_total': (self.baseFare-non_ref_fare)+refunded_taxes,
@@ -47,6 +72,12 @@ class Turkish:
             'currency': self.currencies
         }
         return output
+
+    def __non_refundable_taxes(self, taxes):
+        amount = 0
+        for tax in taxes:
+            amount += tax["Amount"]
+        return amount
 
     def __get_Exchange_Rates(self, data):
         site = requests.get('https://prodengi.kz/currency/')
